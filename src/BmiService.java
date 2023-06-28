28bc0cbdee3b783494bc6f719f2f16a8e24a8d40
Valeriа <Lerusya.Pan@yandex.ru>
2023-06-28 20:38:15 +0700

    All my changes

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,6 +1,6 @@
 public class BmiService {
     public int calculate(double height, double weight) {
-        double index = weight / (height * 2);
+        double index = weight / (height * height);
         return (int) index; // возвращаем рассчитанный индекс массы тела
     }
 }
